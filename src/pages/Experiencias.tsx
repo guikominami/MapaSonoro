fff62d35540c19df799c362d04550209e949fcd6
guikominami <contato.mudalab@gmail.com>
2025-04-21 11:17:22 +0200

    ajuste mapa icones

--- src/pages/Experiencias.tsx
+++ src/pages/Experiencias.tsx
@@ -8,6 +8,8 @@ import iconBruxas from '../assets/imgs/map/icon-bruxas.png';
 import iconBarao from '../assets/imgs/map/icon-barao.png';
 import iconPadre from '../assets/imgs/map/icon-padre.png';
 import iconNoiva from '../assets/imgs/map/icon-noiva.png';
+import iconDunga from '../assets/imgs/map/icon-dunga.png';
+import iconIgreja from '../assets/imgs/map/icon-igreja.png';
 
 const Experiencias = () => {
   return (
@@ -18,38 +20,36 @@ const Experiencias = () => {
         bg-cover bg-no-repeat h-[690px]
       '
     >
-      <div className=''>
-        <div className='absolute top-3 left-3'>
-          <Link to='/'>
-            <img className='w-22' src={iconVoltar} alt='voltar' />
-          </Link>
+      <div className='pt-2 pl-2'>
+        <Link to='/'>
+          <img className='w-20' src={iconVoltar} alt='voltar' />
+        </Link>
+      </div>
+      <div id='icons' className='grid grid-cols-5'>
+        <div className='mt-4 ml-10 w-20'>
+          <img src={iconCarroca} alt='' />
         </div>
-        <IconMap
-          coordinateTop='18'
-          coordinateleft='10'
-          imageSize='w-22'
-          iconName={iconCarroca}
-        />
-        <IconMap
-          coordinateTop='33'
-          coordinateleft='30'
-          imageSize='w-22'
-          iconName={iconBarao}
-        />
-        <IconMap
-          coordinateTop='33'
-          coordinateleft='30'
-          imageSize='w-22'
-          iconName={iconPadre}
-        />
-        <IconMap
-          coordinateTop='53'
-          coordinateleft='43'
-          imageSize='w-15'
-          iconName={iconNoiva}
-        />
-        <div className='absolute top-53 left-43'>
-          <img className='w-15' src={iconNoiva} alt='voltar' />
+        <div id='bruxa-dunga'>
+          <div className='mt-14 ml-6 w-18'>
+            <img src={iconBruxas} alt='voltar' />
+          </div>
+          <div className='mt-88 ml-6 w-18'>
+            <img src={iconDunga} alt='Dunga Ropdrigues' />
+          </div>
+        </div>
+        <div id='barao-noiva' className='mt-25'>
+          <div className='mt-4'>
+            <img src={iconBarao} alt='voltar' />
+          </div>
+          <div className='mt-1'>
+            <img src={iconNoiva} alt='voltar' />
+          </div>
+        </div>
+        <div className='mt-4'>
+          <img src={iconPadre} alt='voltar' />
+        </div>
+        <div className='mt-4'>
+          <img src={iconIgreja} alt='voltar' />
         </div>
       </div>
     </section>
